dc13f6ce98d7a1c856779cd8867adac00aaa6c70
Peter Yorke <peter@peteryorke.net>
2015-02-06 18:09:12 -0800

    IPNAT Tuning Settings

---
+++ S99nat_ip_tuning.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Apply recommended SmartOS TCP tunings
+#
+
+send_buffer=128000
+receive_buffer=1048576
+
+if [ $(($(ipadm show-prop -c -o current -p send_maxbuf tcp)+0)) -lt ${send_buffer} ]; then
+ipadm set-prop -p send_maxbuf=128000 tcp
+fi
+
+if [ $(($(ipadm show-prop -c -o current -p recv_maxbuf tcp)+0)) -lt ${receive_buffer} ]; then
+ipadm set-prop -p recv_maxbuf=1048576 tcp
+fi
+
+ipf -T ipf_nattable_max=60000
+exit 0
